8ffc3c3b02542fab9c707e23e9546d2c380cae4b
dyje <dyje@oocl.com>
2021-08-05 20:07:16 +0800

    [MarsRover][Jesse] test: add test case:
    should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_0_direction_N_action_M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,23 @@
 package com.afs.tdd;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    void should_return_locationX_0_locationY_1_direction_N_when_execute_command_given_locationX_0_locationY_0_direction_N_action_M() {
+        // given
+        Application currentMarsRover = new Application(new RoverStatus(0, 0, "N"));
+        RoverStatus expectedMarsRover = new RoverStatus(0, 1 ,"N");
+        //when
+        currentMarsRover.executeCommand("M");
+        //then
+        assertThat(expectedMarsRover.getLocationX()).isEqualTo((currentMarsRover.getRoverStatus().getLocationX()));
+        assertThat(expectedMarsRover.getLocationY()).isEqualTo((currentMarsRover.getRoverStatus().getLocationY()));
+        assertThat(expectedMarsRover.getDirection()).isEqualTo((currentMarsRover.getRoverStatus().getDirection()));
     }
+
 }
